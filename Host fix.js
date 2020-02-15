6aa8aa21271ce3dbcc9fc92bf13591ac808be54d
Spiccyboyyyy <61081103+Spiccyboyyyy@users.noreply.github.com>
2020-02-15 15:08:48 +0200

    Add files via upload

---
+++ Host fix.js
@@ -0,0 +1,96 @@
+import js.encorev8
+import vpn.asset/ip.port
+import transfer.local_files
+import hostfix/
+import encoreV8
+import oldjs
+import sourcecode
+
+oldjs.require semicolon;
+setActive oldjs.requres semicolon false
+
+data.host{
+data.enCore_Library:(url , href="https://cabconmodding.com/jssource/sourcecode")
+type: (oldjs, undefined url);
+type: (enCore, undefined url);
+oldjs.semicolon == false
+host.ip == 205.76.200.229 
+}
+
+import enCore_library
+import hostname == zIndex
+
+mp == true
+map.addons == true
+get.function():
+{
+online.srvers/port.ip:	205.76.200.229
+
+define.function(ip_type):
+{
+	ip == x;
+	if ip == x;
+		y = get(online.servers)
+		xyz = [x , y]
+		y ++ (xyz-x)
+		y ++ ip.type(z = IPv4I)
+}
+
+ip_type(port.ip/z/y/x)
+z = vpn.asset/ip.port = (,IPv4I,)
+port.ip = z
+
+}	
+Crack = port ++ crack_ports.dll
+def(tranfer) v = [Crack, transfer]
+
+current_ver == en.core.five/ve2928/workshop.steam.dll
+
+import transfer
+import js.encorev8
+
+data. oldjs:(url , href="https://github.com/encore/oldjs/sourcecode")
+type: (oldjs, undefined url);
+
+import oldjs
+import vpn.asset/ip.port
+
+export transfer.content 
+to SourceCode.js
+
+oldjs.v1.9.7/
+
+def crackport.list(Nuketown...):  {
+	
+Crack NT = 6.174.155.113;
+Cracked TDM = 127.58.150.160;
+Nuke69 = 249.41.161.31;
+Eldoro = 152.76.122.130;
+RenDiv = 227.78.117.153;
+PirCoD = 229.163.205.122;
+	
+}
+
+define.frameElement(hostfix):{
+	
+	setActive crackport();	 
+	vpn = false;
+	export valueOf vpn = crack_ports;
+	import crack_ports;
+	
+	export crackport.list.valEldoro toUTCString vpn = 152.76.122.130;
+}
+
+call element(hostfix);
+
+returnValue x - (hostfix + 205.76.200.229);
+
+connect vpn toGMTString var.Eldoro 152.76.122.130;
+
+reverse hostfix();
+call element(hostfix - crackport);
+createEventObject(crackport.fix){
+	
+	cod.refresh();
+	var updateInterval
+}
